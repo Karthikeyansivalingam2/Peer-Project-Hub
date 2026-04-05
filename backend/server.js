69e3d07ab1449d52d78979c446ac7bd7074367c3
Karthikeyansivalingam2 <karthikeyansivalingam2006@gmail.com>
2026-04-05 17:09:45 +0530

    fix: make backend vercel compatible

--- backend/server.js
+++ backend/server.js
@@ -24,5 +24,4 @@ const authRoutes = require('./routes/auth');
 app.use('/api/projects', projectRoutes);
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
